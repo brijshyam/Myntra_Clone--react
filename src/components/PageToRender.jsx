0479c99cc2c593883bdeca70df4340a0e80eb571
brijshyam <brijshyam51maurya@outlook.com>
2023-05-06 14:47:55 +0530

    updated code

--- src/components/PageToRender.jsx
+++ src/components/PageToRender.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/App.css";
 import logo from "../icon.jpeg";
 import cartSvg from "../shopping-cart-solid.svg";
 import prodcutData from "../../data";
-
-function PageToRender() {
+import Product from "./Product";
+const PageToRender = () => {
     const [selectedSort, setSelectedSort] = useState(0);
     const [shoppingCart, setShoppingCart] = useState([]);
     const [modalShirt, setModalShirt] = useState({});
@@ -213,44 +213,8 @@ function PageToRender() {
         });
         return a;
     };
-    function Product(props) {
-        let data = props.data;
-        return (
-            <div
-                className={"indiv-tile-holder"}
-                onClick={() => {
-                    props.clickHandler(data);
-                }}
-            >
-                {/* <div>{JSON.stringify(data)}</div> */}
-                <div className={"indiv-img-holder"}>
-                    <img width={"200px"} src={data.otherImages[0]}></img>
-                </div>
-                <div className={"indiv-tile-metaData"}>
-                    <h4>{data.name}</h4>
-                    <div style={{ fontSize: "0.9em", textOverflow: "clip" }}>
-                        <i>{data.description}</i>
-                    </div>
-                    <div>
-                        {(data.finalPrice ? "Rs." + data.finalPrice : " ") +
-                            " "}
-                        <strike>
-                            <div
-                                style={{ fontSize: "0.8em", display: "inline" }}
-                            >
-                                {data.strickPrice}
-                            </div>
-                        </strike>
-                        <i style={{ color: "red" }}>
-                            {data.discount ? data.discount + "% OFF" : " "}
-                        </i>
-                    </div>
-                </div>
-            </div>
-        );
-    }
     return (
-        <>
+        <div id="main">
             {/* ===========================Product-Modal===================================== */}
             <div
                 id="product-modal-wrapper"
@@ -408,8 +372,8 @@ function PageToRender() {
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
-}
+};
 
 export default PageToRender;
